c3e1057a04408f5a8e35ff1a5b1544bf82007ccf
Buster <yahyemohamed2002@gmail.com>
2024-11-26 21:41:20 -0600

    fixed unorganized folders

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      },
-    },
+    host: '0.0.0.0', // Binds to 0.0.0.0 during development
+    port: 3000       // Port 3000 for consistency with Render's requirements
   },
+  preview: {
+    host: '0.0.0.0', // Binds to 0.0.0.0 during preview
+    port: 3000       // Port 3000 for preview mode
+  }
 });
